e835ed2b4fd4b25391567b4d4b337766da2b55b9
Amen Moja Ra <mojaray2k@me.com>
2012-09-18 12:17:50 -0400

    This is the folder that holds the files of the app that Donnie Wilcox Built

---
+++ samhsa-universal-app-ui/docs/universal-ui-boilerplate/js/script.js
@@ -0,0 +1,82 @@
+/* Author:Donnie Wilcox
+
+*/
+function CreateStickyHeaders(){
+
+	var clonedHeaderRow;
+
+    //$(".persist-area").each(function () {
+//        clonedHeaderRow = $(".persist-header", this);
+//        clonedHeaderRow.before(clonedHeaderRow.clone()).css("width", clonedHeaderRow.innerWidth()).addClass("floatingHeader");
+//
+//    });
+	
+	
+	$(".persist-area").each(function () {
+		
+		$(".floatingHeader").remove();
+		
+		$(".persist-area thead").append('<tr class="floatingHeader persist-header" style="width:100%" />');
+
+        clonedHeader = $(".persist-header th", this);
+		
+		clonedHeader.each(function(){
+			clonedHeader.css("width", clonedHeader.width());
+			})
+		
+        $(".persist-area .floatingHeader").append(clonedHeader.clone());
+
+    });
+
+    $(window).scroll(UpdateTableHeaders).trigger("scroll");}
+function UpdateTableHeaders() {
+    $(".persist-area").each(function () {
+
+        var el = $(this),
+            offset = el.offset(),
+            scrollTop = $(window).scrollTop(),
+            floatingHeader = $(".floatingHeader", this)
+
+            if ((scrollTop > offset.top -77) && (scrollTop < offset.top + el.height())) {
+                floatingHeader.css({
+                    "visibility": "visible"
+                });
+            } else {
+                floatingHeader.css({
+                    "visibility": "hidden"
+                });
+            };
+    });
+}
+;
+(function () {
+    $('#screens-nav a').popover();
+	$('.tool-tip').tooltip({trigger: 'focus'});
+	
+	
+	
+	CreateStickyHeaders();
+	$(window).resize(function(){CreateStickyHeaders()});
+	
+	
+	$('#myTab a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+    });
+	
+	
+	
+	$('#search-form').on('hidden', function () {
+		$("[data-target=#search-form]").html('Open Search' );
+		$('.search-info').show();
+
+    });
+	$('#search-form').on('show', function () {
+    $("[data-target=#search-form]").html('Close Search' );
+	$('.search-info').hide();
+    })
+
+
+}());
+
+
